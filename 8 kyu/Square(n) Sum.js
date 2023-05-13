5b94a241b4f35df15b5185a4587295346a3c5390
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-13 08:31:00 +0100

    Add files via upload

---
+++ 8 kyu/Square(n) Sum.js
@@ -0,0 +1,14 @@
+function squareSum(numbers) {
+  // Create a new array of squared nums
+  let squaredNums = numbers.map((num) => {
+    return num * num
+  })
+
+  // sum all of the new nums together
+  let sum = squaredNums.reduce((acc, num) => {
+    return acc + num
+  }, 0)
+  return sum
+}
+const arr = [1, 2, 3, 4, 5]
+console.log(squareSum(arr))
