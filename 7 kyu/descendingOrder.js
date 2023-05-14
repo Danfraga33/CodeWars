a8d09bacc141a3428c9e3a8a91df3a6a34f6ec43
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-14 07:04:59 +0100

    Create descendingOrder.js

---
+++ 7 kyu/descendingOrder.js
@@ -0,0 +1,22 @@
+function descendingOrder(n){
+  // Broken Down into an array to target single values
+   const splited = String(n).split('')
+   
+   // fn created for use on sort
+   function compareDigits (a, b) {
+     return b - a;
+   }
+ 
+   const arr = []
+   //only take positive integers
+   for (let i = 0; i < splited.length; i++) {
+     if (splited[i] >= 0) {
+       arr.push(splited[i])
+      }
+    }
+    
+   // splited is sorted according to criteria in compareDigits(descending)
+   let cheese = arr.sort(compareDigits)
+   return Number(cheese.join(''))
+   
+  }
