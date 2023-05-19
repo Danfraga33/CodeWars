ba17b6b5470da16d9a5f6b9d5d47381ca54f9cc6
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-19 10:38:24 +0100

    Create findNextSquare.js

---
+++ 7 kyu/findNextSquare.js
@@ -0,0 +1,8 @@
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    // If a sq === sq*sq, THEN RETURN (sq + 1)*sq
+    if (Number.isInteger(Math.sqrt(sq))) {
+      return (Math.sqrt(sq) + 1) * (Math.sqrt(sq)+1)
+    } else return -1
+}
+  
