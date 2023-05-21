57219811678b26182ac6dadc8a62293bcc8de8e3
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-21 14:17:36 +0100

    Create rentalCarCost.js

---
+++ 8 kyu/rentalCarCost.js
@@ -0,0 +1,12 @@
+function rentalCarCost(d) {
+  // Your solution here
+  let totalCost;
+  if (d <= 2) {
+    totalCost = d * 40
+  } else if (d >= 7) {
+    totalCost = (d * 40) - 50
+  } else if (d < 7 && d >= 3) {
+    totalCost = (d * 40) - 20
+  }
+  return totalCost
+}
