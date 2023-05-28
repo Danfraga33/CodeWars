289049cf5304eff572ddf8bf6ee6a514a245c579
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-28 11:20:13 +0100

    Create Your order, please.js

---
+++ 6 kyu/Your order, please.js
@@ -0,0 +1,24 @@
+function order (words) {
+ 
+  let arranged = []
+  let splitedText = words.split(" ")
+  splitedText.sort((a, b) => {
+    const numA = extractNumbericValue(a);
+    const numB = extractNumbericValue(b);
+    return numA - numB
+  })
+  return splitedText.join(" ")
+}
+
+
+const extractNumbericValue = function (word) {
+  let numericValue =""
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    if (!isNaN(char) && char !== " ") {
+      numericValue += char
+    }
+  }
+  return parseInt(numericValue);
+}
