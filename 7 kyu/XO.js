d982646d1dc6c9736c54e776d322c9a8165270ad
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-05-31 13:48:54 +0100

    Create XO.js

---
+++ 7 kyu/XO.js
@@ -0,0 +1,19 @@
+function XO (str) {
+  const splitted = str.split("")
+  let exes = 0;
+  let zeros = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    if (splitted[i].toLowerCase() === 'x') {
+      exes++;
+    } else if (splitted[i].toLowerCase() === 'o') {
+      zeros++;
+    }
+  }
+  if (exes === zeros || (exes === 0 && zeros === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(XO('xoo'))
