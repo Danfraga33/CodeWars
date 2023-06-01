03453fb1456fcc7ffc5536cc5e7d128c7a7e2735
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-06-01 07:41:49 +0100

    Create sumDigPow.js

---
+++ 6 kyu/sumDigPow.js
@@ -0,0 +1,23 @@
+function sumDigPow (a, b) {
+  let result = []
+  for (let i = a; i < b; i++) {
+    if (isDigitPower(i)) {
+      result.push(i)
+    }
+  }
+
+  return result
+}
+
+
+function isDigitPower (num) {
+  let sum = 0;
+  const digits = num.toString().split('');
+
+  for (let i = 0; i < digits.length; i++) {
+    sum += Math.pow(parseInt(digits[i]), i + 1)
+  }
+  return sum === num
+}
+
+console.log(sumDigPow(1,100))
