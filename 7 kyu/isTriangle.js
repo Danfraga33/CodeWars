eaa2c122a9382a95183bd731e07fe371643136ef
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-06-02 14:24:12 +0100

    Create isTriangle.js

---
+++ 7 kyu/isTriangle.js
@@ -0,0 +1,9 @@
+function isTriangle(a,b,c)
+{ // Triangle Theory = sum of lengths of two sides > one side.
+  if (Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c) && a > 0 && b > 0 && c > 0) {
+    if (a + b > c && a + c > b && c + b > a) {
+      return true
+    } else return false 
+  }
+  return false
+}
