85ae5bcd69b4455d9af53b20743ad2d16ee5cd81
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-06-08 12:24:23 +0100

    Create getDivisorsCnt.js

---
+++ 7 kyu/getDivisorsCnt.js
@@ -0,0 +1,9 @@
+function getDivisorsCnt (n) {
+  let divisions = 500000;
+  let count = 0;
+  for (let i = 1; i <= divisions; i++) {
+    n % i === 0 ? count++ : 0  
+  }
+  return count
+  // todo
+}
