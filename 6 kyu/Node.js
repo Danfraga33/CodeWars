81a7f1d77dd735458da679f9cd7f6289c492781e
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-06-26 11:14:31 +0100

    Create Node.js

---
+++ 6 kyu/Node.js
@@ -0,0 +1,36 @@
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function insertNth(head, index, data) {
+  if (index < 0) {
+    throw new Error("Invalid index value: index must be non-negative.");
+  }
+
+  if (index === 0) {
+    const newNode = new Node(data);
+    newNode.next = head;
+    return newNode;
+  }
+
+  let current = head;
+  let previous = null;
+  let count = 0;
+
+  while (count < index && current !== null) {
+    previous = current;
+    current = current.next;
+    count++;
+  }
+
+  if (count < index) {
+    throw new Error("Invalid index value: index is larger than the length of the list.");
+  }
+
+  const newNode = new Node(data);
+  newNode.next = current;
+  previous.next = newNode;
+
+  return head;
+}
