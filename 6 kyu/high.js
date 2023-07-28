7769a38f1de4af4f7788033fecb0f061c38c4196
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-07-28 09:09:02 +0100

    Update high.js

--- 6 kyu/high.js
+++ 6 kyu/high.js
@@ -6,7 +6,7 @@ function high(x){
   for (let i = 0; i < wordList.length; i++) {
     let word = wordList[i]
     let score = 0
-
+ 
     for (let j = 0; j < word.length; j++) {
       let char = word.charAt(j);
       score += char.charCodeAt(0) - 96
