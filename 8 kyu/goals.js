8480c6c16e015ed46a848b777616c6c041cb4436
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-09-27 08:09:02 +0100

    Update goals.js

--- 8 kyu/goals.js
+++ 8 kyu/goals.js
@@ -1,4 +1,4 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   let totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals
-  return totalGoals
+  return totalGoals 
 }
