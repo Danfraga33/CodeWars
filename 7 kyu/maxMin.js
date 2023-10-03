a2d018cd1f0e1b92399f503c327059d6b09c7330
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-10-03 08:02:26 +0100

    Update maxMin.js

--- 7 kyu/maxMin.js
+++ 7 kyu/maxMin.js
@@ -3,7 +3,7 @@ function maxMin (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
   let answer = Math.abs(arr1[i] - arr2[i]) 
   subtraction.push(answer)
-  }
+  } 
   let total = []
   total.push(Math.max(...subtraction))
   total.push(Math.min(...subtraction))
