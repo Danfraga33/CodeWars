f5999d0368120814c3eab4dad406775101818d1c
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2023-11-05 11:50:44 +0000

    Update isPalindrome.js

--- 8 kyu/isPalindrome.js
+++ 8 kyu/isPalindrome.js
@@ -1,6 +1,5 @@
 function isPalindrome(x) {
   const stringL = x.toLowerCase();
   let splitted = stringL.split('').reverse().join('');
-  const result = (splitted === stringL) ? true : false
-  return result
-}
+  return (splitted === stringL) ? true : false
+} 
