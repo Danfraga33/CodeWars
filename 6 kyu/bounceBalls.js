fdd07fa88ff9b13a203a4356900a794b5bd129b8
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2024-01-28 13:29:18 +0000

    Create bounceBalls.js

---
+++ 6 kyu/bounceBalls.js
@@ -0,0 +1,19 @@
+function bouncingBall(h,  bounce,  window) {
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >=  h) {
+    return -1
+  }
+  
+  let timesTheBallIsSeen = 0;
+  let  bounceH = h;
+  console.log(bounceH)
+  while (bounceH > window) {
+    timesTheBallIsSeen += 1
+    bounceH *= bounce
+    // console.log(bounceH)
+    if(bounceH > window) {
+      
+      timesTheBallIsSeen += 1
+    }
+  }
+   return timesTheBallIsSeen
+}
