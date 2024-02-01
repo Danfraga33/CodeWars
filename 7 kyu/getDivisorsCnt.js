b2426dcd053e6771fafb6a422796fcbe23503d54
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2024-02-01 18:51:05 +0000

    Update getDivisorsCnt.js

--- 7 kyu/getDivisorsCnt.js
+++ 7 kyu/getDivisorsCnt.js
@@ -1,7 +1,7 @@
 function getDivisorsCnt (n) {
   let divisions = 500000;
   let count = 0;
-  for (let i = 1; i <= divisions; i++) {
+  for (let i = 1; i <= divisions; i++) { 
     n % i === 0 ? count++ : 0  
   }
   return count 
