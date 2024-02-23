bfb33fc54505e36b450362af4dc8ef7669b3df25
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2024-02-23 22:33:53 +0000

    Update domainName.js

--- 5 kyu/domainName.js
+++ 5 kyu/domainName.js
@@ -2,7 +2,7 @@ function domainName(url) {
   let domainName = url.split('//').pop().split('www.').pop().split('.').shift()
   return domainName;    
 }           
-            
+             
          
     
  
